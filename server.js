bf2a1c1ca45e34c64203b61220a3c8ebd9e60a52
kalyanikapse94 <30695612+KalyaniKapse@users.noreply.github.com>
2017-08-09 20:01:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-1', function (req, res) {
+  res.send("Article one is served here.");
+});
+
+app.get('article-2', function (req, res) {
+  res.send("Article two is served here.");
+});
+
+app.get('article-3', function (req, res) {
+  res.send("Article three is served here.");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

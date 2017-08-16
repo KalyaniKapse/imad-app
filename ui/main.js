71b3321ae5f704de691f5ccb3e108266f14cc5cd
kalyanikapse94 <30695612+KalyaniKapse@users.noreply.github.com>
2017-08-16 19:48:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,5 +19,21 @@ button.onclick=function(){
   //make request
   request.open('GET','http://coco98.imad.hasura-app.io/counter',true);
   request.send(null);
-    
+};
+
+//submit name
+var nameInput=document.getElementById('name');
+var name= nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+  //make request to the server and send the name
+  
+  //capture a list of names and render it as a list
+  var names=['name1','name2','name3','name4'];
+  var list='';
+  for(var i=0;i<names.lenght;i++){
+      list+='<li>'+name[i]+'</li>';
+  }
+  var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
 };
